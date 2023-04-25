e11dedb56d7a5e9f058f500c17120410877aea36
KunalSingh <65989201+KunalSingh11@users.noreply.github.com>
2023-04-25 12:51:46 +0530

    Add files via upload

---
+++ 5-forEachDemo/forEachDemo/forEachDemo.js
@@ -0,0 +1,22 @@
+import { LightningElement } from 'lwc';
+
+export default class ForEachDemo extends LightningElement {
+
+    List = [
+        {
+            id: 1,
+            Name: "A",
+            Age: "24"
+        },
+        {
+            id: 2,
+            Name: "B",
+            Age: "25"
+        },
+        {
+            id: 3,
+            Name: "C",
+            Age: "26"
+        }
+    ];
+}
---
+++ 6-iteratorDemo/iteratorDemo/iteratorDemo.js
@@ -0,0 +1,21 @@
+import { LightningElement } from 'lwc';
+
+export default class IteratorDemo extends LightningElement {
+    List = [
+        {
+            id: 1,
+            Name: "A",
+            Age: "24"
+        },
+        {
+            id: 2,
+            Name: "B",
+            Age: "25"
+        },
+        {
+            id: 3,
+            Name: "C",
+            Age: "26"
+        }
+    ];
+}
